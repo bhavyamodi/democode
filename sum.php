947e83376033c321791dac9f41e96598529176d8
Bhavya Modi <modi.bhavya88@gmail.com>
2021-12-04 23:51:55 +0530

    added function

--- sum.php
+++ sum.php
@@ -1,4 +1,5 @@
 <?php
+require_once("difference.php");
 
 function sum($a , $b){
     $c = $a + $b;
@@ -7,6 +8,9 @@ function sum($a , $b){
 
 $d = sum(1,2);
 echo $d;
+
+$differ = diff(2,6);
+echo $differ;
 
 $a = 1;
 $b = 2;
